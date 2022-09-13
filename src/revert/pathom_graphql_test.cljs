3b59929b1bfb6e10fa9869fd8c46c316a999209e
Gonzalo Rafael Acosta <gacosta.r89@gmail.com>
2022-09-12 19:11:37 -0500

    Bump pathom3 versions
    - Mark the issue as fixed

--- src/revert/pathom_graphql_test.cljs
+++ src/revert/pathom_graphql_test.cljs
@@ -1,14 +1,12 @@
 (ns revert.pathom-graphql-test
   (:require
    [cljs.pprint :as pprint]
+   [com.wsscode.pathom3.connect.planner :as pcp]
    [com.wsscode.pathom3.connect.built-in.resolvers :as pbir]
    [com.wsscode.pathom3.connect.indexes :as pci]
    [com.wsscode.pathom3.graphql :as p.gql]
    [com.wsscode.pathom3.interface.async.eql :as p.eql]
    [promesa.core :as p]))
-
-
-
 
 ;;;; staker
 ;;
@@ -26,16 +24,28 @@
           js-data (.json resp)]
     (js->clj js-data)))
 
+(def env (-> {::pcp/experimental-branch-optimizations true}
+             (pci/register
+              [(pbir/equivalence-resolver :staker.Incentive/rewardToken :univ3.Token/id)])
+             (p.gql/connect-graphql
+              {::p.gql/namespace "univ3"}
+              #(request-subgraph % "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-polygon"))
+             (p.gql/connect-graphql
+              {::p.gql/namespace "staker"}
+              #(request-subgraph % "https://api.thegraph.com/subgraphs/name/revert-finance/uni-v3-vesting-staker-polygon"))))
+
+(p/let [env env]
+  (tap> env))
+
+(p/let [result (p.eql/process
+                env
+                [{'(:staker.Query/incentives {:first 10})
+                  [:staker.Incentive/id
+                   :staker.Incentive/contract
+                   :univ3.Token/symbol]}])]
+  (pprint/pprint result))
+
 (comment
-  (def env (-> {}
-               (pci/register
-                [(pbir/equivalence-resolver :staker.Incentive/rewardToken :univ3.Token/id)])
-               (p.gql/connect-graphql
-                {::p.gql/namespace "univ3"}
-                #(request-subgraph % "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-polygon"))
-               (p.gql/connect-graphql
-                {::p.gql/namespace "staker"}
-                #(request-subgraph % "https://api.thegraph.com/subgraphs/name/revert-finance/uni-v3-vesting-staker-polygon"))))
 
   (p/let [env env]
     (tap> env))
@@ -58,7 +68,7 @@
                      :univ3.Token/symbol]}])]
     (pprint/pprint result))
 
-  ;; FAILS
+  ;; FIXED
   (p/let [result (p.eql/process
                   env
                   {:staker.Incentive/id "0x0bb698b687540a7f44504cf1063a4977d2351f201f18fc50281def0f36349d2e"}
@@ -66,14 +76,11 @@
                    :univ3.Token/symbol])]
     (pprint/pprint result))
 
-  ;; ALSO FAILS
+  ;; FIXED
   (p/let [result (p.eql/process
                   env
                   {:staker.Incentive/id "0x0bb698b687540a7f44504cf1063a4977d2351f201f18fc50281def0f36349d2e"}
                   [:staker.Incentive/contract])]
-    (pprint/pprint result))
-
-  )
-
+    (pprint/pprint result)))
 
 (defn init [] (js/console.log "hello"))
